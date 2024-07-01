234cb4c967ea84cb928db28d318dc4b6dd4c5fda
Tomek <01169258@pw.edu.pl>
2024-07-01 20:59:08 +0200

    Database setup for elements.

---
+++ database/mysqlsetup.sh
@@ -0,0 +1,13 @@
+sudo systemctl start mysql
+
+sudo mysql -u root -e "CREATE DATABASE LEGO;"
+
+sudo mysql -u root LEGO < database_init.sql
+
+sudo mysql -u root LEGO < sql/colors.sql
+sudo mysql -u root LEGO < sql/sets.sql
+sudo mysql -u root LEGO < sql/parts.sql
+sudo mysql -u root LEGO < sql/inventories.sql
+sudo mysql -u root LEGO < sql/inventory_parts.sql
+sudo mysql -u root LEGO < sql/elements.sql
+
