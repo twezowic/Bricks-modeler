ea9b3659e16e692e4a2c2135299a1a81397e8334
Tomasz Wężowicz <t.wezowicz7@gmail.com>
2024-12-11 22:49:51 +0100

    Add bottom depth map.

--- api/database/depth_map.py
+++ api/database/depth_map.py
@@ -67,7 +67,84 @@ def get_size(file_name: str):
     return size
 
 
-def depth_map_from_faces(file_num: int, generate_images=False):
+def depth_map_bottom(file_num: int, generate_images=False):
+    file_name = f'./api/database/gltf/{file_num}.gltf'
+
+    size = get_size(file_name)
+
+    mesh = trimesh.load(file_name)
+    vertices = np.array(mesh.geometry[f'{file_num}-Mesh'].vertices)
+    faces = np.array(mesh.geometry[f'{file_num}-Mesh'].faces)
+
+    width, height, z_value = size
+    depth_map = np.full((height, width), np.inf)
+
+    # znormalizowanie vertexów na mapę
+    max_coords = np.max(vertices, axis=0)
+    min_coords = np.min(vertices, axis=0)
+
+    vertices = (vertices - min_coords) / (max_coords - min_coords)
+
+    vertices[:, 0] = width * vertices[:, 0] - 1
+    vertices[:, 1] = height * vertices[:, 1] - 1
+
+    # wypełnienie depth mapy
+    for face in faces:
+        p1, p2, p3 = vertices[face]
+        x_coords = [int(p1[0]), int(p2[0]), int(p3[0])]
+        y_coords = [int(p1[1]), int(p2[1]), int(p3[1])]
+        min_x, max_x = max(min(x_coords), 0), min(max(x_coords), width - 1)
+        min_y, max_y = max(min(y_coords), 0), min(max(y_coords), height - 1)
+
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                if is_point_in_triangle(x, y, p1, p2, p3):
+                    depth_map[y, x] = 1
+    depth_map[np.isinf(depth_map)] = 0
+    depth_map_normalized = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
+    depth_map_normalized = depth_map_normalized.astype(np.uint8)
+
+    print(np.min(depth_map))
+
+    if generate_images:
+        output_image = cv2.cvtColor(depth_map_normalized.astype(np.uint8), cv2.COLOR_GRAY2BGR)
+    _, binary_image = cv2.threshold(depth_map_normalized, 128, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    inside_points = []
+    if len(contours) > 0:
+        largest_contour = max(contours, key=cv2.contourArea)
+        
+        epsilon = 0.02 * cv2.arcLength(largest_contour, True)
+        approx_polygon = cv2.approxPolyDP(largest_contour, epsilon, True)
+
+        possible_insets = []
+        for x in range(10, width, 20):
+            for y in range(10, height, 20):
+                possible_insets.append([x, y])
+        for point in possible_insets:
+            minimum = (point[0]-3, point[1]+3)
+            maximum = (point[0]+3, point[1]-3)
+            if cv2.pointPolygonTest(largest_contour, tuple(minimum), False) >= 0 and cv2.pointPolygonTest(largest_contour, tuple(maximum), False) >= 0:
+                inside_points.append((point[0], point[1], 0))
+                if generate_images:
+                    cv2.circle(output_image, tuple(point), 3, (0, 0, 255), -1)
+        if generate_images:
+            cv2.drawContours(output_image, [approx_polygon], -1, (0, 255, 255), thickness=2)
+    elif np.min(depth_map) == 1:
+        for x in range(10, width, 20):
+            for y in range(10, height, 20):
+                inside_points.append([x, y, 0])
+                if generate_images:
+                    cv2.circle(output_image, (x, y), 3, (0, 0, 255), -1)
+
+    if generate_images:    
+        cv2.imwrite(f'depth_map/faces/bot_{file_num}.png', output_image)
+
+    return inside_points
+
+
+def depth_map_top(file_num: int, generate_images=False):
     file_name = f'./api/database/gltf/{file_num}.gltf'
 
     size = get_size(file_name)
@@ -92,7 +169,7 @@ def depth_map_from_faces(file_num: int, generate_images=False):
     vertices = (vertices - min_coords) / (max_coords - min_coords)
 
     vertices[:, 0] = width * vertices[:, 0] - 1
-    vertices[:, 1] = height - 1 - (height * vertices[:, 1])  # sprawdzić czy na pewno w bazie też jest odwrócone
+    vertices[:, 1] = height * vertices[:, 1] - 1
 
     # wypełnienie depth mapy
     for face in faces:
@@ -124,7 +201,7 @@ def depth_map_from_faces(file_num: int, generate_images=False):
 
     gradient_magnitude_normalized = cv2.normalize(gradient_magnitude, None, 0, 255, cv2.NORM_MINMAX)
     gradient_magnitude_normalized = gradient_magnitude_normalized.astype(np.uint8)
-
+depth_map_top
     # do usunięcia
     # _, binary_image = cv2.threshold(gradient_magnitude_normalized, 100, 255, cv2.THRESH_BINARY)
     # binary_image = cv2.Canny(gradient_magnitude_normalized, 50, 150)
@@ -144,29 +221,52 @@ def depth_map_from_faces(file_num: int, generate_images=False):
             if generate_images:
                 cv2.drawContours(output_image, [contour],
                                  -1, (0, 0, 255), 1)
-            insets.add((floor(x/40), floor(y/40),
-                        (ceil(depth_map[int(y), int(x)] * z_value)-4) // 8
+            insets.add((floor(x/40)*10+10, floor(y/40)*10+10,
+                        (ceil(depth_map[int(y), int(x)] * z_value)-4)
                         ))
 
         elif generate_images:
             cv2.drawContours(output_image, [contour],
                              -1, (0, 255, 0), 1)
     if generate_images:
-        cv2.imwrite(f'depth_map/faces/x/{file_num}.png', output_image)
-    print(sorted(insets))
+        cv2.imwrite(f'depth_map/faces/top_{file_num}.png', output_image)
+    return sorted(insets)
 
 
 def generate(n=1543):
     with open('./api/database/new_parts.csv', mode='r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for index, row in track(enumerate(reader), total=n):
-            depth_map_from_faces(row['part_num'])
+            generate_top_bottom_insets(row['part_num'])
             if index == n:
                 break
 
 
-# generate(15)
-depth_map_from_faces("26599")
+def generate_top_bottom_insets(file_num: str):
+    bottom = depth_map_bottom(file_num)
+    top = depth_map_top(file_num)
+
+    file_name = f"depth_map/csv/{file_num}_bot.csv"
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(bottom)
+    file_name = f"depth_map/csv/{file_num}_top.csv"
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(top)
 
 
-# todo wybrać jakieś podstawowe i przetestować czy to działa na różnych 10 elementach
+example = [
+    "122c01",
+    "2345",
+    "2339",
+    "2401",
+    "2466",
+    "3001",
+    "3003",
+    "30151",
+    "28802",
+]
+for i in example:
+    print(i)
+    generate_top_bottom_insets(i)
