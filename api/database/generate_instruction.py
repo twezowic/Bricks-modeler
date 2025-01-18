05a144f14f1009c1b58395ece6c365cd0144bf6b
Tomasz Wężowicz <t.wezowicz7@gmail.com>
2025-01-18 18:22:58 +0100

    Change generating to max number step.

--- api/database/generate_instruction.py
+++ api/database/generate_instruction.py
@@ -4,7 +4,7 @@ import networkx as nx
 
 
 def generate_stepdb(models: List[ModelDB], connections: List[ConnectionDB],
-                    min_models_in_step: int = 3) -> List[StepDB]:
+                    max_models_in_step: int = 3) -> List[StepDB]:
     # Uzupełnienie grafu skierowanego
     graph = nx.DiGraph()
 
@@ -23,8 +23,8 @@ def generate_stepdb(models: List[ModelDB], connections: List[ConnectionDB],
         bottom_models = [node for node in graph.nodes if graph.in_degree(node) == 0]
         if len(bottom_models) == len(graph.nodes):  
             save_step = True 
-        for i in range(0, len(bottom_models), min_models_in_step):
-            batch_models = bottom_models[i:i + min_models_in_step]
+        for i in range(0, len(bottom_models), max_models_in_step):
+            batch_models = bottom_models[i:i + max_models_in_step]
 
             current_step['connection'].extend(
                 [graph.edges[edge]["connection"]
@@ -37,7 +37,7 @@ def generate_stepdb(models: List[ModelDB], connections: List[ConnectionDB],
                 [graph.nodes[model_id]["model"] for model_id in batch_models]
             )
 
-            if save_step or len(current_step['models']) >= min_models_in_step:
+            if save_step or len(current_step['models']) >= max_models_in_step:
                 filtered_connections = [
                         connection for connection in current_step['connection']
                         if any(connection.down_id == model.model_id for model in current_step['models'])
@@ -45,17 +45,17 @@ def generate_stepdb(models: List[ModelDB], connections: List[ConnectionDB],
 
                 steps.append(StepDB(
                     step=step_number,
-                    models=current_step['models'],
+                    models=current_step['models'][:max_models_in_step],
                     connections=filtered_connections
                 ))
                 step_number += 1
-                current_step['models'] = []
+                current_step['models'] = current_step['models'][max_models_in_step:]
                 current_step['connection'] = [connection for connection in current_step['connection']
                                               if connection not in filtered_connections]
                 save_step = False
             else:
                 save_step = True
 
-            # Usuwanie przetworzonych modeli z grafu
+            #  Usuwanie przetworzonych modeli z grafu
             graph.remove_nodes_from(batch_models)
     return steps
--- frontend/src/Builder/Instruction.js
+++ frontend/src/Builder/Instruction.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { IconButton } from '@mui/material';
 import { ArrowForward, ArrowBack } from '@mui/icons-material';
 
 const Instruction = ({ images, correctSteps, currentStep, setCurrentStep }) => {
@@ -16,53 +15,33 @@ const Instruction = ({ images, correctSteps, currentStep, setCurrentStep }) => {
     };
 
     return (
-            <div className='absolute top-20 right-12 items-center justify-between flex gap-2 border-2 border-black z-10 bg-white'>
-                <div className='flex flex-col'>
-                <img
-                    src={images[currentStep]}
-                    alt={`carousel-image-${currentStep}`}
-                    className='w-[400px] h-auto'
-                />
-                <div className='flex flex-row justify-between items-center px-4'>
-                    <div className={`w-8 h-8  rounded-full ${correctSteps ? "bg-green-400" : "bg-red-600"}`}/>
-                    <span className='text-[16px] px-4 py-2'>
-                        {currentStep + 1} / {images.length}
-                     </span>
-                </div>
-                </div> 
-            <IconButton
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '10px',
-                    transform: 'translateY(-50%)',
-                    zIndex: 1,
-                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
-                    '&:hover': {
-                        backgroundColor: 'rgba(255, 255, 255, 0.9)',
-                    },
-                }}
-                onClick={prevImage}
-            >
-                <ArrowBack />
-            </IconButton>
-            <IconButton
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    right: '10px',
-                    transform: 'translateY(-50%)',
-                    zIndex: 1,
-                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
-                    '&:hover': {
-                        backgroundColor: 'rgba(255, 255, 255, 0.9)',
-                    },
-                }}
-                onClick={nextImage}
-            >
-                <ArrowForward />
-            </IconButton>
+    <div className="absolute top-20 right-12 flex items-center justify-between gap-2 border-2 border-black z-10 bg-white">
+        <div className="flex flex-col">
+            <img
+            src={images[currentStep]}
+            alt={`carousel-image-${currentStep}`}
+            className="w-[400px] h-auto"
+            />
+            <div className="flex flex-row justify-between items-center px-4">
+            <div className={`w-8 h-8 rounded-full ${correctSteps ? "bg-green-400" : "bg-red-600"}`} />
+            <span className="text-[16px] px-4 py-2">
+                {currentStep + 1} / {images.length}
+            </span>
             </div>
+        </div>
+        <button
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 z-10 bg-white/70 hover:bg-white/90 rounded-full p-2"
+            onClick={prevImage}
+        >
+            <ArrowBack />
+        </button>
+        <button
+            className="absolute top-1/2 right-2 transform -translate-y-1/2 z-10 bg-white/70 hover:bg-white/90 rounded-full p-2"
+            onClick={nextImage}
+        >
+            <ArrowForward />
+        </button>
+    </div>
     );
 };
 
